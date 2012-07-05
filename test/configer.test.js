e9124a72dbc5580a5f6643a6ec2287603fe4539e
aleafs <zhangxc83@gmail.com>
2012-07-05 13:53:40 +0800

    Add test case for lib/configer.js

--- test/configer.test.js
+++ test/configer.test.js
@@ -3,6 +3,42 @@
 var should  = require('should');
 var config  = require(__dirname + '/../lib/configer.js');
 
+var _storer = function (data) {
+
+  var _me   = {};
+  _me.get   = function (key, callback) {
+    callback(key.indexOf('error') > -1 ? new Error('TestError') : null, data[key]);
+  };
+
+  return _me;
+};
+
 describe('configer interface test', function () {
+
+  /* {{{ should_configer_get_value_works_fine() */
+  it('should_configer_get_value_works_fine', function (done) {
+
+    var num = 2;
+    var _me = config.create('///app1/app2/', _storer({
+    '/app1/app2/key1' : 'AbCd1我asd'
+  }));
+
+    _me.get('key1', null, function (error, data) {
+      should.ok(!error);
+      data.should.eql('AbCd1我asd');
+      if ((--num) === 0) {
+        done();
+      }
+    });
+
+    _me.get('error', null, function (error, data) {
+      error.toString().should.eql('Error: TestError');
+      if ((--num) === 0) {
+        done();
+      }
+    });
+  });
+  /* }}} */
+
 });
 
