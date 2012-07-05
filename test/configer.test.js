0572b69708171621cca2d39e2dd182e4f4e20316
aleafs <zhangxc83@gmail.com>
2012-07-05 13:12:31 +0800

    Add test/ scripts

---
+++ test/configer.test.js
@@ -0,0 +1,8 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+var should  = require('should');
+var config  = require(__dirname + '/../lib/configer.js');
+
+describe('configer interface test', function () {
+});
+
