a70933a2a7ea47097a73e9b626a45a4991ce2cb8
aleafs <zhangxc83@gmail.com>
2012-07-09 18:16:41 +0800

    Add config test case for number and ini

--- test/config.test.js
+++ test/config.test.js
@@ -23,14 +23,36 @@ describe('configer interface test', function () {
   /* {{{ should_configer_get_value_works_fine() */
   it('should_configer_get_value_works_fine', function (done) {
 
-    var num = 2;
+    var num = 4;
     var _me = config.create('///app1/app2/', _storer({
-    '/app1/app2/key1' : 'AbCd1我asd'
+    '/app1/app2/key1' : 'AbCd1我asd',
+    '/app1/app2/key2' : '-123.3123',
+    '/app1/app2/key3' : '; this is comment\na = "b"\n[section1]\n\n\n\na = -1231.3  ',
+    '/app3/app2/key1' : 'abcd',
   }));
 
     _me.get('key1', null, function (error, data) {
       should.ok(!error);
       data.should.eql('AbCd1我asd');
+      if ((--num) === 0) {
+        done();
+      }
+    });
+
+    _me.get('key2', 'number', function (error, data) {
+      should.ok(!error);
+      data.should.eql(-123.3123);
+      if ((--num) === 0) {
+        done();
+      }
+    });
+
+    _me.get('key3', 'ini', function (error, data) {
+      should.ok(!error);
+      JSON.stringify(data).should.eql(JSON.stringify({
+        'a' : 'b',
+        'section1' : {'a' : -1231.3}
+      }));
       if ((--num) === 0) {
         done();
       }
