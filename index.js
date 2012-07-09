086183d9787e59c78e9d845ae7427c99df3b2e0f
aleafs <zhangxc83@gmail.com>
2012-07-09 16:29:28 +0800

    iservice api redefined

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
+var Emitter = require('events').EventEmitter;
+var Util    = require('util');
+
 exports.createClient = function (options) {
 
-  var _me = {};
+  var Client = function () {
+    Emitter.call(this);
+  };
+  Util.inherits(Client, Emitter);
 
-  return _me;
+  /**
+   * @zookeeper object
+   */
+  var zk = require(__dirname + '/lib/zookeeper.js').create(options);
+
+  Client.prototype.createConfig = function (prefix) {
+    return require(__dirname + '/lib/config.js').create(prefix, zk);
+  };
+
+  return new Client();
 };
 
--- test/config.test.js
+++ test/config.test.js
@@ -1,7 +1,7 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
 var should  = require('should');
-var config  = require(__dirname + '/../lib/configer.js');
+var config  = require(__dirname + '/../lib/config.js');
 
 var _storer = function (data) {
 
