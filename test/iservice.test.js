46710df7de88b0ae3a93d55fff2316db6fb0b3b8
aleafs <zhangxc83@gmail.com>
2012-07-27 21:14:19 +0800

    lalala

--- test/iservice.test.js
+++ test/iservice.test.js
@@ -3,15 +3,70 @@
 var fs      = require('fs');
 var should  = require('should');
 
-var http    = require('http').createServer(function (req, res) {
-  res.end('hello world');
+/* {{{ mocked iservice server */
+
+var __mockeddata = {
+  '/test/key1'  : {
+    'data'  : 1234,
+    'meta'  : 1,
+  },
+  '/test/key2'  : {
+    'data'  : 5678,
+    'meta'  : 2,
+  },
+  '/test/key1/aa'   : {
+    'data'  : 'abcd',
+    'meta'  : 2,
+  },
+  '/test'   : {
+    'data'  : '周华健',
+    'meta'  : 1,
+  },
+};
+
+var http = require('http').createServer(function (req, res) {
+  var url = req.url.split('/');
+  var ctl = url.shift();
+
+  if ('api' === ctl) {
+    var key = url.join('/');
+    switch (url.shift()) {
+      case 'get':
+        res.write(JSON.stringify({
+          'error'   : null,
+          'data'    : __mockeddata[key],
+        }));
+        break;
+
+      case 'watch':
+        break;
+
+      case 'tree':
+        res.write(JSON.stringify({
+          'error'   : null,
+          'data'    : __mockeddata,
+        }));
+        break;
+
+      case 'feedback':
+        break;
+
+      default:
+        res.write('undefined action.');
+        break;
+    }
+  }
+
+  res.end(req.url);
 }).listen(33750);
+
+/* }}} */
 
 describe('iservice connect interface', function () {
 
   /* {{{ client object */
   var client = require(__dirname + '/../lib/iservice.js').create({
-    'hosts' : '127.0.0.1:33750,127.0.0.2',
+    'hosts' : '127.0.0.1:33750',
       'root'    : 'test',
       'user'    : 'unittest',
       'pass'    : '123456',
@@ -23,7 +78,8 @@ describe('iservice connect interface', function () {
   /* {{{ should_client_get_works_fine() */
   it('should_client_get_works_fine', function (done) {
     client.get('/test/key1', function (error, data) {
-      console.log(error);
+      should.ok(!error);
+      console.log(data.toString());
       done();
     });
   });
