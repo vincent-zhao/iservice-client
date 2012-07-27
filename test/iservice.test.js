232bc2743154ebf8c58ae598bbe542dbc87d343f
aleafs <zhangxc83@gmail.com>
2012-07-27 18:01:21 +0800

    remove zookeeper test

--- test/iservice.test.js
+++ test/iservice.test.js
@@ -1,13 +1,31 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
+var fs      = require('fs');
 var should  = require('should');
-var service = require(__dirname + '/../');
 
-describe('iservice interface', function () {
+var http    = require('http').createServer(function (req, res) {
+  res.end('hello world');
+}).listen(33749);
 
-  it('should_iservice_create_client_works_fine', function () {
-    var _me = service.createClient();
+describe('iservice connect interface', function () {
+
+  /* {{{ client object */
+  var client = require(__dirname + '/../lib/iservice.js').create({
+    'hosts' : '127.0.0.1,127.0.0.2',
+      'root'    : 'test',
+      'user'    : 'unittest',
+      'pass'    : '123456',
+      'cache'   : __dirname + '/../run/cache',
+      'uuid'    : 'test',
+  });
+  /* }}} */
+
+  it('should_client_get_works_fine', function () {
   });
 
 });
 
+after(function () {
+  http.close();
+  http = null;
+});
