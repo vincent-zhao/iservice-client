d1dc13c440fb26f54b738a1f652def69d0809135
aleafs <zhangxc83@gmail.com>
2012-09-04 14:12:30 +0800

    Public API modified

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
-var Emitter = require('events').EventEmitter;
-var Util    = require('util');
+"use strict";
 
-exports.createClient = function (options) {
+var options = null;
+var storage = null;
+var connect = function () {
+  if (!storage) {
+    storage = require(__dirname + '/lib/iservice.js').create(options);
+  }
+  return storage;
+};
 
-  var store = require(__dirname + '/lib/iservice.js').create(options);
+exports.init = function (config) {
+  options = config;
+};
 
-  var Client = function () {
-    Emitter.call(this);
-  };
-  Util.inherits(Client, Emitter);
-
-  Client.prototype.setEventHandle = function (evt, callback) {
-    this.on(evt, callback);
-  };
-
-  Client.prototype.createConfig = function (prefix, conf) {
-    return require(__dirname + '/lib/config.js').create(prefix, conf, store);
-  };
-
-  var _me = new Client();
-
-  return _me;
+exports.createConfig = function (prefix, config) {
+  return require(__dirname + '/lib/config.js').create(prefix, config, connect());
 };
 
