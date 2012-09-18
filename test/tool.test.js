64a072edd4c9437d25c5a2ceaeeac88a10603957
弈轩 <yixuan.zzq@taobao.com>
2012-09-19 02:05:04 +0800

    fix some process write cache bug

--- test/tool.test.js
+++ test/tool.test.js
@@ -1,6 +1,15 @@
 var fs      = require('fs');
 var should  = require('should');
 var exec    = require('child_process').exec;
+var tool    = require(__dirname + '/../lib/tool.js');
+
+/*{{{ beforeEach */
+beforeEach(function (done) {
+  cleanUp(function () {
+    done();
+  });
+});
+/*}}}*/
 
 describe('tool test', function () {
 
@@ -12,7 +21,7 @@ describe('tool test', function () {
     fs.mkdirSync(root + '/dir_test/dir2');
     fs.writeFileSync(root + '/dir_test/file1');
     fs.writeFileSync(root + '/dir_test/dir1/file2');
-    require(__dirname + '/../lib/tool.js').rmdir(root + '/dir_test');
+    tool.rmdir(root + '/dir_test');
     try {
       fs.statSync(root + '/dir_test');
     } catch(e) {
@@ -21,4 +30,109 @@ describe('tool test', function () {
   });
   /*}}}*/
 
+  /*{{{ should_getpids_works_fine() */
+  it('should_getpids_works_fine', function (done) {
+    var file = __dirname + '/run/tmp.js';
+    var content = 
+      'var tool = require(__dirname + \'/../../lib/tool.js\');' +
+      'setTimeout(function(){tool.getPids(function(error, pids){console.log(pids);});},1000);' + 
+      'setTimeout(function(){},3000);';
+    fs.writeFileSync(file, content);
+    var command = 'node ' + file;
+    var count = 2;
+    exec(command, function (error, stdout, stderr) {
+      stdout.split(',').length.should.eql(2);
+      if (--count === 0) {
+        done();
+      }
+    });
+    exec(command, function (error, stdout, stderr) {
+      stdout.split(',').length.should.eql(2);
+      if (--count === 0) {
+        done();
+      }
+    });
+  });
+  /*}}}*/
+
+  /*{{{ should_dump_no_need_to_copy_works_fine() */
+  it('should_dump_no_need_to_copy_works_fine', function (done) {
+    fs.mkdirSync(__dirname + '/run/1');
+    fs.writeFileSync(__dirname + '/run/1/test', 'lalala');
+    fs.mkdirSync(__dirname + '/run/2');
+    fs.writeFileSync(__dirname + '/run/2/test', 'lalala2');
+
+    var file = __dirname + '/run/tmp.js';
+    var content = 
+      'var tool = require(__dirname + \'/../../lib/tool.js\');' +
+      'setTimeout(function(){tool.dump(__dirname, false, function(){console.log(\'finished!\');});},1000);' + 
+      'setTimeout(function(){},3000);';
+    fs.writeFileSync(file, content);
+    var command = 'node ' + file;
+    var count = 2;
+    for(var i = 0;i < 2; i++) {
+      exec(command, function (error, stdout, stderr) {
+        if (--count === 0) {
+          try {
+            fs.statSync(__dirname + '/run/1');
+          } catch(e) {
+            fs.statSync(__dirname + '/run/2');
+            done();
+          }
+        }
+      });
+    }
+  });
+  /*}}}*/
+
+  /*{{{ should_dump_need_to_copy_works_fine() */
+  it('should_dump_need_to_copy_works_fine', function (done) {
+    fs.mkdirSync(__dirname + '/run/1');
+    fs.writeFileSync(__dirname + '/run/1/test', 'lalala');
+    setTimeout(function(){
+      fs.mkdirSync(__dirname + '/run/2');
+      fs.writeFileSync(__dirname + '/run/2/test', 'lalala2');
+
+      var file = __dirname + '/run/tmp.js';
+      var content = 
+        'var tool = require(__dirname + \'/../../lib/tool.js\');' +
+        'setTimeout(function(){tool.dump(__dirname, true, function(){console.log(\'finished!\');});},1000);' + 
+        'setTimeout(function(){},3000);';
+      fs.writeFileSync(file, content);
+      var command = 'node ' + file;
+      var count = 2;
+      for(var i = 0;i < 2; i++) {
+        exec(command, function (error, stdout, stderr) {
+          if (--count === 0) {
+            try {
+              fs.statSync(__dirname + '/run/1');
+            } catch(e) {
+              fs.statSync(__dirname + '/run/2');
+              var folders = fs.readdirSync(__dirname + '/run');
+              var num = 0;
+              for (var i = 0; i < folders.length; i++) {
+                if (folders[i] !== '2' && fs.statSync(__dirname + '/run/' + folders[i]).isDirectory()) {
+                  num++;
+                  fs.readFileSync(__dirname + '/run/' + folders[i] + '/test').toString().should.eql('lalala2');
+                }
+              }
+              num.should.eql(2);
+              done();
+            }
+          }
+        });
+      }
+    },2000);
+  });
+  /*}}}*/
+
 });
+
+/*{{{ cleanUp() */
+function cleanUp(callback){
+  exec('rm -rf ' + __dirname + '/run/*', function (err, out, stderr) {
+    callback();
+  });
+}
+/*}}}*/
+
